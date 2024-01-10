3e875580937dada690b3cb0092afccd379353093
IbraSuraya <hasannn.9c@gmail.com>
2024-01-11 04:43:46 +0700

    3 scenario done 01-03

--- cypress/e2e/pageHome/section-menu1.cy.js
+++ cypress/e2e/pageHome/section-menu1.cy.js
@@ -1,19 +1,56 @@
 describe("Page - Home", () => {
+  const randomDelay = Cypress._.random(100, 500)
+
   beforeEach(() => {
     cy.on('uncaught:exception', (err, runnable) => {
       return false
-    })
+    });
     cy.visit('/', {
       headers: {
         "Accept-Encoding": "gzip, deflate, br",
       },
     });
-    cy.url().should("include", "/")
+    cy.wait(randomDelay)
   })
 
   context("Section - Menu", () => {
-    it("Access Home Page", () => {
-      cy.get('[data-testid="icnHeaderIcon"] > img')
+    it("TP-PH-01 Kembali ke home page", () => {
+      cy.get('[data-testid="icnHeaderIcon"] > img').click().wait(randomDelay)
+      cy.url().should("include", "/").wait(randomDelay)
     })
+    
+    it("TP-PH-02 Download mobile-apps via website", () => {
+      cy.get('.css-5b0cy').trigger('mouseover')
+      cy.get('#qrCodeEl').should('be.visible')
+      cy.get('[style="margin-top: 9px;"] > a > img').should('be.visible')
+      cy.get('[style="margin-top: 8px;"] > a > img').should('be.visible')
+      cy.get('.css-5b0cy').invoke('removeAttr', 'target').click().wait(randomDelay)
+      cy.url().should("include", "/mobile-apps").wait(randomDelay)
+    })
+    
+    it("TP-PH-03 Download mobile-apps via playstore", () => {
+      cy.get('.css-5b0cy').should('be.visible').trigger('mouseover');
+      cy.get('[style="margin-top: 9px;"] > a').should('be.visible').invoke('removeAttr', 'target').click().wait(randomDelay)
+      cy.url().should("eq", "https://play.google.com/store/apps/details?id=com.tokopedia.tkpd").wait(randomDelay)
+    })
+    
+    it("TP-PH-06XX", () => {
+      cy.get('[data-testid="btnHeaderAbout"]').invoke('removeAttr', 'target').click().wait(randomDelay)
+      cy.url().should("include", "/about")
+      
+    })
+    // it.only("TP-PH-02 Download mobiel-apps via website", () => {
+    //   cy.get('[data-testid="headerText"]').trigger('mouseover').wait(randomDelay)
+    //   // cy.get('[data-testid="headerText"]').invoke('show')
+
+    //   cy.get('[data-testid="btnHeaderCategory#1"] > div').click()
+    //   cy.url().should("include", "/p/rumah-tangga", {
+    //     headers: {
+    //       "Accept-Encoding": "gzip, deflate, br",
+    //     },
+    //   })
+    //   // cy.get('[data-testid="showHide#1"]').trigger('mouseover')
+    // })
+    
   })
 })
---
+++ cypress/utils/image.js
@@ -0,0 +1,7 @@
+let dataImage = {
+  mobileApps_qrCode: 	'https://assets.tokopedia.net/assets-tokopedia-lite/v2/zeus/kratos/cea272dc.svg',
+  mobileApps_playstore: '	https://assets.tokopedia.net/assets-tokopedia-lite/v2/zeus/kratos/e25ec508.svg',
+  mobileApps_appstore: 'https://assets.tokopedia.net/assets-tokopedia-lite/v2/zeus/kratos/4cc56a99.svg'
+}
+
+export {dataImage}
