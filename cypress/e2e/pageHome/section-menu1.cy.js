32acf8955fec730f511402ad88a8c768d424102e
IbraSuraya <hasannn.9c@gmail.com>
2024-01-12 22:18:46 +0700

    non progress create code scap data for get all category all segement

--- cypress/e2e/pageHome/section-menu1.cy.js
+++ cypress/e2e/pageHome/section-menu1.cy.js
@@ -1,8 +1,11 @@
 import {dataUrl} from '../../utils/dataUrl.js'
 import { dataImage } from '../../utils/dataImage.js';
 
+function randomDelay(){
+  return Cypress._.random(100, 500)
+}
+
 describe("Page - Home", () => {
-  const randomDelay = Cypress._.random(100, 500)
   
   beforeEach(() => {
     cy.on('uncaught:exception', (err, runnable) => {
@@ -13,7 +16,7 @@ describe("Page - Home", () => {
           "Accept-Encoding": "gzip, deflate, br",
         },
     });
-    cy.wait(randomDelay)
+    cy.wait(randomDelay())
   })
 
   context("Section - Secondary Menu", () => {
@@ -22,20 +25,20 @@ describe("Page - Home", () => {
       cy.get('#qrCodeEl').should('be.visible')
       cy.get('[style="margin-top: 9px;"] > a > img').should('be.visible')
       cy.get('[style="margin-top: 8px;"] > a > img').should('be.visible')
-      cy.get('.css-5b0cy').invoke('removeAttr', 'target').click().wait(randomDelay)
-      cy.url().should("include", dataUrl.mobileApps).wait(randomDelay)
+      cy.get('.css-5b0cy').invoke('removeAttr', 'target').click().wait(randomDelay())
+      cy.url().should("include", dataUrl.mobileApps).wait(randomDelay())
     })
     
     it("TP-PH-03 Download mobile-apps via playstore", () => {
       cy.get('.css-5b0cy').should('be.visible').trigger('mouseover');
-      cy.get('[style="margin-top: 9px;"] > a').should('be.visible').invoke('removeAttr', 'target').click().wait(randomDelay)
-      cy.url().should("eq", dataUrl.playstore).wait(randomDelay)
+      cy.get('[style="margin-top: 9px;"] > a').should('be.visible').invoke('removeAttr', 'target').click().wait(randomDelay())
+      cy.url().should("eq", dataUrl.playstore).wait(randomDelay())
     })
     
     it("TP-PH-04 Download mobile-apps via appstore", () => {
       cy.get('.css-5b0cy').should('be.visible').trigger('mouseover');
-      cy.get('[style="margin-top: 8px;"] > a').should('be.visible').invoke('removeAttr', 'target').click().wait(randomDelay)
-      cy.url().should("eq", dataUrl.appstore).wait(randomDelay)
+      cy.get('[style="margin-top: 8px;"] > a').should('be.visible').invoke('removeAttr', 'target').click().wait(randomDelay())
+      cy.url().should("eq", dataUrl.appstore).wait(randomDelay())
     })
     
     it("TP-PH-05 Download mobile-apps via QR Code", () => {
@@ -46,35 +49,35 @@ describe("Page - Home", () => {
     })
     
     it("TP-PH-06 Redirect page about", () => {
-      cy.get('[data-testid="btnHeaderAbout"]').invoke('removeAttr', 'target').click().wait(randomDelay)
-      cy.url().should("include", dataUrl.about).wait(randomDelay)  
+      cy.get('[data-testid="btnHeaderAbout"]').invoke('removeAttr', 'target').click().wait(randomDelay())
+      cy.url().should("include", dataUrl.about).wait(randomDelay())  
     })
     
     it("TP-PH-07 Redirect page Mitra Tokopedia", () => {
-      cy.get('[data-testid="btnHeaderMitra"]').invoke('removeAttr', 'target').click().wait(randomDelay)
-      cy.url().should("include", dataUrl.mitra).wait(randomDelay)  
+      cy.get('[data-testid="btnHeaderMitra"]').invoke('removeAttr', 'target').click().wait(randomDelay())
+      cy.url().should("include", dataUrl.mitra).wait(randomDelay())  
     })
     
     it("TP-PH-08 Redirect page Mulai Berjualan", () => {
-      cy.get('[data-testid="btnHeaderSellerEdu"]').invoke('removeAttr', 'target').click().wait(randomDelay)
-      cy.url().should("include", dataUrl.mulaiBerjualan).wait(randomDelay)  
+      cy.get('[data-testid="btnHeaderSellerEdu"]').invoke('removeAttr', 'target').click().wait(randomDelay())
+      cy.url().should("include", dataUrl.mulaiBerjualan).wait(randomDelay())  
     })
     
-    it.only("TP-PH-09 Redirect page Promo", () => {
-      cy.get('[data-testid="btnHeaderPromo"]').invoke('removeAttr', 'target').click().wait(randomDelay)
-      cy.url().should("include", dataUrl.promo[1]).wait(randomDelay)  
+    it("TP-PH-09 Redirect page Promo", () => {
+      cy.get('[data-testid="btnHeaderPromo"]').invoke('removeAttr', 'target').click().wait(randomDelay())
+      cy.url().should("include", dataUrl.promo[1]).wait(randomDelay())  
     })
     
     it("TP-PH-10 Redirect page Tokopedia Care", () => {
-      cy.get('[data-testid="btnHeaderHelp"]').invoke('removeAttr', 'target').click().wait(randomDelay)
-      cy.url().should("include", dataUrl.help).wait(randomDelay)  
+      cy.get('[data-testid="btnHeaderHelp"]').invoke('removeAttr', 'target').click().wait(randomDelay())
+      cy.url().should("include", dataUrl.help).wait(randomDelay())  
     })
   })
     
   context("Section - Main Menu", () => {
     it("TP-PH-01 Kembali ke home page", () => {
-      cy.get('[data-testid="icnHeaderIcon"] > img').click().wait(randomDelay)
-      cy.url().should("include", "/").wait(randomDelay)
+      cy.get('[data-testid="icnHeaderIcon"] > img').click().wait(randomDelay())
+      cy.url().should("include", "/").wait(randomDelay())
     })
   })
 })
--- cypress/e2e/scrapingData.cy.js
+++ cypress/e2e/scrapingData.cy.js
@@ -1,23 +1,50 @@
-// Mendefinisikan data hasil scraping
-let listSubCategory = [];
+function randomDelay(){
+  return Cypress._.random(100, 500)
+}
 
 describe('Scraping Data', () => {
-  const randomDelay = Cypress._.random(100, 500)
-
+  
   beforeEach(() => {
     cy.on('uncaught:exception', (err, runnable) => {
-        return false
+      return false
     });
     cy.visit('/', {
       headers: {
-          "Accept-Encoding": "gzip, deflate, br",
-        },
+        "Accept-Encoding": "gzip, deflate, br",
+      },
     });
-    cy.wait(randomDelay)
+    cy.wait(randomDelay())
+  })
+  
+  it.only('All Category', () => {
+    // Mendefinisikan data hasil scraping
+    let tempAll = []
+    // Get button kategori
+    cy.get('[data-testid="headerText"]').should('be.visible').trigger('mouseover');
+    
+    // Mengambil elemen semua kategori
+    cy.get('[data-testid="allCategoryTab"]').within(() => {
+      // Ambil data atribute tiap tag div
+      cy.get('div').each((data) => {
+        // const tempText = cy.get(`[data-testid="${data.attr('data-testid')}"] div`).invoke('text')
+        let tempId = data.attr('data-testid')
+        // let tempText = cy.get(`[data-testid="${tempId}"] div`).invoke('text')
+        const temp = {
+          testId: tempId,
+          text: cy.get('div').invoke('text')
+        };
+        tempAll.push(temp)
+      }).wait(randomDelay());
+    });
+    cy.log(tempAll)
+    // cy.log(JSON.stringify(tempAll, null, 2));
+    // cy.writeFile('cypress/fixtures/data_allCat.json', tempAll).wait(randomDelay())
   })
 
-  it.only('All Sub Category', () => {
+  it('All Sub Category Belanja', () => {
+    // Get button kategori
     cy.get('[data-testid="headerText"]').should('be.visible').trigger('mouseover');
+
 
     // Mengambil elemen dengan data-testid "allCategories"
     cy.get('[data-testid="allCategories"]').within(() => {
@@ -31,7 +58,7 @@ describe('Scraping Data', () => {
         };
 
         // Menambahkan data ke array hasil scraping
-        listSubCategory.push(categoryData);
+        tempAll.push(categoryData);
       });
     });
 
@@ -47,7 +74,7 @@ describe('Scraping Data', () => {
     cy.fixture('data_subCategory').then((subCategorys) => {
       subCategorys.forEach(_element => {
         console.log(_element.subCategory)
-        cy.get(`[data-testid="${_element.subCategory}"]`).wait(randomDelay)
+        cy.get(`[data-testid="${_element.subCategory}"]`).wait(randomDelay())
       });
     })
   })
