385248ff7f8f17e891458a15832ce1c8427d07f1
IbraSuraya <hasannn.9c@gmail.com>
2024-01-15 11:19:39 +0700

    Done scenario get all page sub cateogry from category belanja

--- cypress/e2e/pageHome/section-menu1.cy.js
+++ cypress/e2e/pageHome/section-menu1.cy.js
@@ -2,6 +2,7 @@ import {dataUrl} from '../../utils/dataUrl.js'
 import { dataImage } from '../../utils/dataImage.js';
 
 function randomDelay(){
+  // return Cypress._.random(500, 800)
   return Cypress._.random(100, 500)
 }
 
@@ -75,9 +76,121 @@ describe("Page - Home", () => {
   })
     
   context("Section - Main Menu", () => {
+    function navToSubCategory(category, subCat, linkSubCat) {
+      // Get every sub-cat
+      cy.get(`[data-testid="${subCat}"]`).trigger('mouseover').wait(randomDelay()).click();
+      cy.url().should("include", linkSubCat).wait(randomDelay())
+      cy.go(-1)
+      cy.get(`[data-testid="headerText"]`).should('be.visible').trigger('mouseover').wait(randomDelay());
+      cy.get(`[data-testid="${category}"] div`).click().wait(randomDelay());
+    }
+
     it("TP-PH-01 Kembali ke home page", () => {
       cy.get('[data-testid="icnHeaderIcon"] > img').click().wait(randomDelay())
       cy.url().should("include", "/").wait(randomDelay())
+    })
+    
+    // IT IS RECOMMENDED TO DO DATA FETCHING AGAIN SO THAT IT IS UPDATED.
+    it("TP-PH-11.A (1-5) Redirect page every sub category from category Belanja", () => {
+      // btn "Kategori"
+      cy.get(`[data-testid="headerText"]`).should('be.visible').trigger('mouseover').wait(randomDelay());
+      cy.fixture('data_allCat').then((allCat) => {
+        // Category "Belanja"
+        const _belanja = allCat[0]
+        cy.get(`[data-testid="${_belanja.id}"] div`).click().wait(randomDelay());
+        
+        // Access All sub-cat
+        _belanja.subCats.forEach((_subCat, idxSubCat) => {
+          if (idxSubCat >= 5) return    // Sub-Cat 1-5
+          cy.log("INDEX ", idxSubCat)
+          navToSubCategory(_belanja.id, _subCat.id, _subCat.href)
+        })
+      })
+    })
+    
+    it("TP-PH-11.B (6-10) Redirect page every sub category from category Belanja", () => {
+      // btn "Kategori"
+      cy.get(`[data-testid="headerText"]`).should('be.visible').trigger('mouseover').wait(randomDelay());
+      cy.fixture('data_allCat').then((allCat) => {
+        // Category "Belanja"
+        const _belanja = allCat[0]
+        cy.get(`[data-testid="${_belanja.id}"] div`).click().wait(randomDelay());
+        
+        // Access All sub-cat
+        _belanja.subCats.forEach((_subCat, idxSubCat) => {
+          if (idxSubCat < 5 || idxSubCat >= 10) return    // Sub-Cat 6-10
+          cy.log("INDEX ", idxSubCat)
+          navToSubCategory(_belanja.id, _subCat.id, _subCat.href)
+        })
+      })
+    })
+    
+    it("TP-PH-11.C (11-15) Redirect page every sub category from category Belanja", () => {
+      // btn "Kategori"
+      cy.get(`[data-testid="headerText"]`).should('be.visible').trigger('mouseover').wait(randomDelay());
+      cy.fixture('data_allCat').then((allCat) => {
+        // Category "Belanja"
+        const _belanja = allCat[0]
+        cy.get(`[data-testid="${_belanja.id}"] div`).click().wait(randomDelay());
+        
+        // Access All sub-cat
+        _belanja.subCats.forEach((_subCat, idxSubCat) => {
+          if (idxSubCat < 10 || idxSubCat >= 15) return    // Sub-Cat 11-15
+          cy.log("INDEX ", idxSubCat)
+          navToSubCategory(_belanja.id, _subCat.id, _subCat.href)
+        })
+      })
+    })
+    
+    it("TP-PH-11.D (16-20) Redirect page every sub category from category Belanja", () => {
+      // btn "Kategori"
+      cy.get(`[data-testid="headerText"]`).should('be.visible').trigger('mouseover').wait(randomDelay());
+      cy.fixture('data_allCat').then((allCat) => {
+        // Category "Belanja"
+        const _belanja = allCat[0]
+        cy.get(`[data-testid="${_belanja.id}"] div`).click().wait(randomDelay());
+        
+        // Access All sub-cat
+        _belanja.subCats.forEach((_subCat, idxSubCat) => {
+          if (idxSubCat < 15 || idxSubCat >= 20) return    // Sub-Cat 16-20
+          cy.log("INDEX ", idxSubCat)
+          navToSubCategory(_belanja.id, _subCat.id, _subCat.href)
+        })
+      })
+    })
+    
+    it("TP-PH-11.E (21-25) Redirect page every sub category from category Belanja", () => {
+      // btn "Kategori"
+      cy.get(`[data-testid="headerText"]`).should('be.visible').trigger('mouseover').wait(randomDelay());
+      cy.fixture('data_allCat').then((allCat) => {
+        // Category "Belanja"
+        const _belanja = allCat[0]
+        cy.get(`[data-testid="${_belanja.id}"] div`).click().wait(randomDelay());
+        
+        // Access All sub-cat
+        _belanja.subCats.forEach((_subCat, idxSubCat) => {
+          if (idxSubCat < 20 || idxSubCat >= 25) return    // Sub-Cat 21-25
+          cy.log("INDEX ", idxSubCat)
+          navToSubCategory(_belanja.id, _subCat.id, _subCat.href)
+        })
+      })
+    })
+    
+    it("TP-PH-11.F (26-30) Redirect page every sub category from category Belanja", () => {
+      // btn "Kategori"
+      cy.get(`[data-testid="headerText"]`).should('be.visible').trigger('mouseover').wait(randomDelay());
+      cy.fixture('data_allCat').then((allCat) => {
+        // Category "Belanja"
+        const _belanja = allCat[0]
+        cy.get(`[data-testid="${_belanja.id}"] div`).click().wait(randomDelay());
+        
+        // Access All sub-cat
+        _belanja.subCats.forEach((_subCat, idxSubCat) => {
+          if (idxSubCat < 25) return    // Sub-Cat 26-30
+          cy.log("INDEX ", idxSubCat)
+          navToSubCategory(_belanja.id, _subCat.id, _subCat.href)
+        })
+      })
     })
   })
 })
